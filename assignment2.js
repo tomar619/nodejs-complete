508c2eef84d4c0c6931948b1823ca83fa83ce7ad
Kumar <Amit.Kumar2@sas.dk>
2022-08-25 14:31:47 +0200

    Complete the assignment 2

---
+++ assignment2.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const { send } = require("process");
+
+const app = express();
+
+app.use('/users', (req, res, next) => {
+    console.log('The Champions of Europe');
+    res.send('<h1>Manchester City, Champions Forever</h1>');
+});
+
+app.use('/', (req, res, next) => {
+    console.log("Hey, welcome to Manchester City");
+    res.send('<h1>Welcome to Manchester City</h1>');
+    //next();
+});
+
+app.listen(3000);
