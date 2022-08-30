b11170500acc2e91e1d8fb325f4b804783d0664b
Kumar <Amit.Kumar2@sas.dk>
2022-08-30 11:32:52 +0200

    Added dynamic content template engine support

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
-//Root modules
 const path = require('path');
 
-//Third party modules
 const express = require('express');
 const bodyParser = require('body-parser');
-
-//Custom Imports
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+//const expressHbs = require('express-handlebars');
 
 const app = express();
+
+//Only for handlebars
+//app.engine('hbs', expressHbs({ layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs' }));
+
+//Set up the template engine to use
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(3000);
