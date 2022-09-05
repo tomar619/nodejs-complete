fb0847dc0dccb2c96e230ce3fd82525229c87d54
Kumar <Amit.Kumar2@sas.dk>
2022-09-05 11:02:03 +0200

    Added dynamic routes and advanced models

---
+++ util/database.js
@@ -0,0 +1,6 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', 
+                                { dialect: 'mysql', host: 'localhost'});
+
+module.exports = sequelize;
